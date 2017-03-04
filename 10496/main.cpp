8cd3e52db413d0c99445fb9ec0c699470eab1218
rozina36 <ruma.36.csedu@gmail.com>
2017-03-05 00:00:57 +0600

    10496 - Collecting Beppers

---
+++ 10496/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  10496
+//
+//  Created by Rozina Akter Ruma on 4/3/17.
+//  Copyright © 2017 Rozina Akter Ruma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
